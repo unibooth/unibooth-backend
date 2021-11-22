910f60b4b5d4bf30f6aa154f4a3f42de7c4ce5a1
Youhoseong <h0song@naver.com>
2021-11-22 22:13:36 +0900

    feat: Project initialize

---
+++ src/main/java/com/unibooth/unibooth/UniboothApplication.java
@@ -0,0 +1,13 @@
+package com.unibooth.unibooth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniboothApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UniboothApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/unibooth/unibooth/UniboothApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unibooth.unibooth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniboothApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
