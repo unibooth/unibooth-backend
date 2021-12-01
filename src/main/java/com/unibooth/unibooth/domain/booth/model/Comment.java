b5c47aa51090dd7107e7ffc380bbfb9d0044d7bb
Youhoseong <h0song@naver.com>
2021-12-02 03:26:01 +0900

    feat: Comment Domain created

---
+++ src/main/java/com/unibooth/unibooth/domain/booth/model/Comment.java
@@ -0,0 +1,22 @@
+package com.unibooth.unibooth.domain.booth.model;
+
+import com.unibooth.unibooth.domain.user.model.User;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@NoArgsConstructor
+@Getter
+public class Comment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String content;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    private User user;
+    private Date createdAt;
+}
